2c653ec1fb089a4a0f4caa12f6c01ab128e25406
Torje Hoås Digernes <torjehoa@pvv.ntnu.no>
2018-03-17 23:06:49 +0100

    Added simple reduc program

---
+++ reduc/main.cpp
@@ -0,0 +1,49 @@
+
+#include <mpi.h>
+#include <Ranges.h>
+#include <iostream>
+#include <cstddef>
+#include <cmath>
+#include "../mach1/node_function.h"
+using namespace std;
+int main(int argc, char  *argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int world_size;
+    MPI_Comm_size( MPI_COMM_WORLD, &world_size );
+
+    int world_rank;
+    MPI_Comm_rank( MPI_COMM_WORLD, &world_rank );
+    if ( 0 == world_rank ){
+	system("date");
+    }
+    if ( argc == 2 ){
+        int glength = stoi(argv[1]);
+        int length = glength/world_size;
+        double *gnums = nullptr;
+        double *nums = new double[length];
+        if ( world_rank == 0){
+            gnums = new double[glength];
+            for ( auto num :  IntDoubleRange(0,glength,1,1.0,201.0,1.0)){
+                gnums[num.first] = num.second;
+            }
+        }
+        MPI_Scatter(gnums, length,MPI_DOUBLE, nums,length, MPI_DOUBLE,0,MPI_COMM_WORLD);
+        double sum = node_function(nums,length);
+	double gsum = 0;
+        MPI_Allreduce(&sum,&gsum,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+        if ( 0 == world_rank){
+	    system("date");
+            cout << "π - π_estimate: " <<M_PI- 4*gsum << endl;
+        }
+
+        MPI_Finalize();
+        return 0;
+
+    }else{
+        cout << "no length specified, exiting"<< endl;
+        MPI_Finalize();
+        exit(0);
+    }
+
+}
